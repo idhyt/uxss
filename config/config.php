503a55c850249de762a5ff3d08958aaad2053332
idhyt <idhyt@hotmail.com>
2016-02-23 10:02:07 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -6,12 +6,12 @@
  * Time: 15:36
  */
 
-$DOMAIN = "http://10.20.230.142/uxss/src";
+$DOMAIN = "http://127.0.0.1/uxss/src";
 
 //数据库配置信息
 $DB_CONFIG_UXSS = array(
     'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => '10.20.230.142', // 服务器地址
+    'DB_HOST'   => '127.0.0.1', // 服务器地址
     'DB_NAME'   => 'vul_info', // 数据库名
     'DB_USER'   => 'root', // 用户名
     'DB_PWD'    => 'root', // 密码
